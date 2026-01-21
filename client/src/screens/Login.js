57922255e9e21d6337089cb83a41c0304f706f27
Sneha Sah <sahsneha2062@gmail.com>
2026-01-21 10:45:44 +0545

    updated client

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { Link } from 'react-router-dom'
+import styles from './Login.module.css'
 
 export default function Login() {
   return (
     <>
       <Navbar/>
-      <div>
+      <div className={`${styles.login}`}>
         <h3>Manager Login</h3>
         <input type="email" placeholder='Email'/>
         <button>Next</button>
--- client/src/screens/Start.js
+++ client/src/screens/Start.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { Link } from 'react-router-dom'
+import styles from "./Start.module.css"
 
 export default function Start() {
   return (
@@ -9,22 +10,22 @@ export default function Start() {
       <h1>Sign up for LIBRARYLYNX</h1>
       <p>Your library catalog available anywhere, anytime.</p>
       <div className="container">
-        <div>
         <h2>Select a Subscription</h2>
-        <div>
+        <div className={`${styles.subscription}`}>
+        <div className={`${styles.subs0}`}>
           <h1>BASIC</h1>
           <p>For an individual's home library.Personal use only, no ads, and we do not sell or share your data.</p>
         </div>
-        <div>
+        <div className={`${styles.subs1}`}>
           <h1>PRO</h1>
           <p>Enjoy powerful features like lending,patron management, and a customizable published library.</p>
         </div>
-        <div>
+        <div className={`${styles.subs2}`}>
           <h1>ULTIMATE</h1>
           <p>Need multiple accounts? More than 25 managers? Need invoicing and POs? Contact Us!</p>
         </div>
         </div>
-        <div>
+        <div className={`${styles.info}`}>
           <h1>Account Info</h1>
           <input type="text" placeholder='First Name' />
           <input type="text" placeholder='Last Name' />
